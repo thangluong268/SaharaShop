5bc703e32b656368c85f3290b4acd2f627a82861
thangluong268 <luongthang730@gmail.com>
2022-03-22 21:53:41 +0700

    Hello

---
+++ WebBanHang/EShop/store/views.py
@@ -0,0 +1,22 @@
+from itertools import product
+from unicodedata import category
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from store.models.category import Category
+from .models.product import Product
+from .models.category import Category
+
+# Create your views here.
+def index(request):
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
